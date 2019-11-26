c2b8ce4cc0e2890851e27e751864574b1ee4ab3c
soldesk <soldesk@soldesk-PC>
2019-11-26 10:13:22 +0900

    test추가

---
+++ spring_hap/src/test/java/spring/model/mapper/Member_hapMapperTest.java
@@ -0,0 +1,24 @@
+package spring.model.mapper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Member_hapMapperTest {
+
+	@Test
+	public void testMember_create() {
+		fail("Not yet implemented");
+	}
+  
+	@Test
+	public void testDuplicatedId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDuplicatedEmail() {
+		fail("Not yet implemented");
+	}
+
+}
